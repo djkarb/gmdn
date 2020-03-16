9be448bac189374e1f6f64bcd04bcde3467b6dc7
andrea <andreacarbone98@libero.it>
2020-03-16 22:35:50 +0100

    Prima bozza layout

--- app/src/main/java/com/example/gmdn/MainActivity.java
+++ app/src/main/java/com/example/gmdn/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.gmdn;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView t;
+    private Button b;
+    private EditText e;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        t = (TextView) findViewById(R.id.displayMessage);
+        b = (Button) findViewById(R.id.enter_button);
+        e = (EditText) findViewById(R.id.input_field);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String messaggio = e.getText().toString();
+                t.setText(messaggio);
+            }
+        });
     }
 }
